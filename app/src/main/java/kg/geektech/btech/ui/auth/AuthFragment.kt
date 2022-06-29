c5381fb72ace21515ea7c0682dff1555fd70c82b
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-06-29 19:15:50 +0600

    Added fonts and AuthFragment (Launch Screen)

--- app/src/main/java/kg/geektech/btech/ui/auth/AuthFragment.kt
+++ app/src/main/java/kg/geektech/btech/ui/auth/AuthFragment.kt
@@ -5,16 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import kg.geektech.btech.R
+import kg.geektech.btech.databinding.FragmentAuthBinding
 
 class AuthFragment : Fragment() {
+
+    private lateinit var binding: FragmentAuthBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+        binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnSignUp.setOnClickListener {
+            navController = Navigation.findNavController(
+                requireActivity(),
+                R.id.nav_host_fragment_activity_main
+            )
+            navController.navigate(R.id.navigation_sign_up)
+        }
+        binding.btnSignIn.setOnClickListener {
+            navController = Navigation.findNavController(
+                requireActivity(),
+                R.id.nav_host_fragment_activity_main
+            )
+            navController.navigate(R.id.navigation_sign_in)
+        }
     }
 
 }

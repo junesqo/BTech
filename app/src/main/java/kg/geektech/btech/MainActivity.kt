4d4af4bb806605428338228746578e370a8d191d
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-06-29 17:55:29 +0600

    Added fragments, bottom navigation bar

--- app/src/main/java/kg/geektech/btech/MainActivity.kt
+++ app/src/main/java/kg/geektech/btech/MainActivity.kt
@@ -2,10 +2,59 @@ package kg.geektech.btech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kg.geektech.btech.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+        navBarViewVisibility()
+    }
+
+    private fun initBottomNavigation() {
+        val navView: BottomNavigationView = binding.navbar
+
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_bestsellers, R.id.navigation_basket, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    private fun navBarViewVisibility() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.navigation_auth ||
+                destination.id == R.id.navigation_sign_in ||
+                destination.id == R.id.navigation_sign_up
+            ) {
+                binding.navbar.visibility = View.GONE
+            } else {
+                binding.navbar.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
---
+++ app/src/main/java/kg/geektech/btech/ui/signIn/SignInFragment.kt
@@ -0,0 +1,19 @@
+package kg.geektech.btech.ui.signIn
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kg.geektech.btech.R
+
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+}

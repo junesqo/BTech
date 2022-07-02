3afb89cd5e3a4031bfeb0f7a23b2126086d38d92
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-07-02 10:01:23 +0600

    ViewModels added

---
+++ app/src/main/java/kg/geektech/btech/ui/bestsellers/BestsellersViewModel.kt
@@ -0,0 +1,6 @@
+package kg.geektech.btech.ui.bestsellers
+
+import kg.geektech.btech.base.BaseViewModel
+
+class BestsellersViewModel: BaseViewModel() {
+}
---
+++ app/src/main/java/kg/geektech/btech/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package kg.geektech.btech.ui.profile
+
+import androidx.lifecycle.ViewModel
+import kg.geektech.btech.base.BaseViewModel
+
+class ProfileViewModel: BaseViewModel() {
+}

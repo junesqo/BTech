006c3ec138afb9ce295dc96a10385cab265eddb9
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-07-28 23:07:56 +0600

    fragment_profile.xml and fragment_favorite.xml added

---
+++ app/src/main/java/kg/geektech/btech/ui/favorite/FavoriteViewModel.kt
@@ -0,0 +1,6 @@
+package kg.geektech.btech.ui.favorite
+
+import kg.geektech.btech.base.BaseViewModel
+
+class FavoriteViewModel: BaseViewModel() {
+}
--- app/src/main/java/kg/geektech/btech/ui/profile/ProfileFragment.kt
+++ app/src/main/java/kg/geektech/btech/ui/profile/ProfileFragment.kt
@@ -3,6 +3,8 @@ package kg.geektech.btech.ui.profile
 import android.view.LayoutInflater
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kg.geektech.btech.R
 import kg.geektech.btech.base.BaseFragment
 import kg.geektech.btech.databinding.FragmentProfileBinding
 
@@ -11,6 +13,16 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel, N
 
     override fun inflateViewBinding(inflater: LayoutInflater): FragmentProfileBinding {
         return FragmentProfileBinding.inflate(inflater)
+    }
+
+    override fun initListener() {
+        binding.btnFavorite.setOnClickListener {
+            navController = Navigation.findNavController(
+                requireActivity(),
+                R.id.nav_host_fragment_activity_main
+            )
+            navController.navigate(R.id.navigation_favorite)
+        }
     }
 
 }
